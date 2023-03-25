a391a6551053fb6329fca9f49d2825af8173d79c
Om Nalamwar <78678547+omnalamwar@users.noreply.github.com>
2023-03-25 23:43:54 +0530

    build run, Added scroll box, used json, used react lifecycle

---
+++ src/containers/App.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+//STATE object which describes application
+
+//PROPS are used to get passed values and variables - props never changes
+//state - props comes out of states		STATE>>props  	- here state is parent and props is child
+
+class App extends Component {
+
+	constructor(){
+		//smart components - because it has states
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+	//react lifecycle methods - 1. constructor 2. componentWillMount 3. render 4. componentDidMount
+
+	compnentWillMount(){
+
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response=> response.json())
+			.then(users =>this.setState({robots:users}));
+	}
+
+	onSearchChange = (event) =>{
+		this.setState({searchfield: event.target.value})
+	}
+
+	render(){
+		const {robots, searchfield} = this.state
+		const filteredRobots = robots.filter(robots => {
+			return robots.name.toLowerCase().includes(searchfield.toLowerCase());
+		})
+
+		return !robots.length ? 
+			<h1>Loading</h1> :
+			<div className='tc'>
+				<h1 className='f1'>Robofriends</h1>
+				<SearchBox searchChange = {this.onSearchChange}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
+			</div>
+	}
+
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'sincere@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'toughy miss',
+		username: 'blind',
+		email: 'toughy@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'phantom',
+		username: 'love',
+		email: 'birds@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Good',
+		username: 'night',
+		email: 'pursuit@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Tree',
+		username: 'green',
+		email: 'health@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Pinkerton',
+		username: 'finder',
+		email: 'color@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Cantervile',
+		username: 'Ghost',
+		email: 'ghost@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'invisible',
+		username: 'class',
+		email: 'wing@gmail.com'
+	},
+	{
+		id: 9,
+		name: 'best',
+		username: 'inclass',
+		email: 'topper@gmail.com'
+	},
+	{
+		id: 10,
+		name: 'good luck',
+		username: 'for',
+		email: 'future@gmail.com'
+	}
+]
